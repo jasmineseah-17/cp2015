7a18c154ab86f9c26a79a2b379c249c621f867a8
jasmineseah-17 <seah.yongyee.jasmine@dhs.sg>
2015-01-30 22:18:24 +0800

    Update q5_determine_prime.py

--- q5_determine_prime.py
+++ q5_determine_prime.py
@@ -16,6 +16,9 @@ for prime in range(1, 1001):
     is_prime(prime)
 
 for x in a:
-    print(a[0:10])
+    print("{0:<5} {1:<5} {2:<5} {3:<5} {4:<5} {5:<5} {6:<5} {7:<5} {8:<5} {9:<5}"
+    .format(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9]))
     del a[0:10]
-print(a)
+    count += 1
+    if count == 100:
+        break
